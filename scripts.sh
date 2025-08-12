59085ab2eff1316f5e068f0a5f2810e954d13535
ZhentaoFan <88043988+ZhentaoFan@users.noreply.github.com>
2025-08-12 17:11:53 -0400

    wandb

--- scripts.sh
+++ scripts.sh
@@ -1,5 +1,6 @@
 apt update
 apt install nano
 pip install -r requirements.txt
+echo 508a8f0c303712b4ed646b4e7aa8179707e5414d | wandb login
 python dataset/build_sudoku_dataset.py --output-dir data/sudoku-extreme-1k-aug-1000  --subsample-size 1000 --num-aug 1000
 OMP_NUM_THREADS=8 torchrun --nproc-per-node 1 pretrain.py data_path=data/sudoku-extreme-1k-aug-1000 epochs=40000 eval_interval=4000 lr=1e-4 puzzle_emb_lr=1e-4 weight_decay=1.0 puzzle_emb_weight_decay=1.0
