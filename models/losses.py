f0fe7ea9103e6a4373769849a0d9f6a746b02974
ZhentaoFan <88043988+ZhentaoFan@users.noreply.github.com>
2025-08-13 13:08:19 -0400

    4 2*

--- models/losses.py
+++ models/losses.py
@@ -98,4 +98,4 @@ class ACTLossHead(nn.Module):
         # Filter outputs for return
         detached_outputs = {k: outputs[k].detach() for k in return_keys if k in outputs}
 
-        return new_carry, lm_loss + 0.5 * (q_halt_loss + q_continue_loss), metrics, detached_outputs, new_carry.halted.all()
+        return new_carry, lm_loss + 0.5 * (2*q_halt_loss + q_continue_loss), metrics, detached_outputs, new_carry.halted.all()
